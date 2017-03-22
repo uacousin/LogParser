b3dc8b8c31d0759d32775ab16eb4338c63fcac67
Oleh Fedianovych <uacousin.stig@outlook.com>
2017-03-22 21:29:09 +0200

    List version

--- LogParser/Program.cs
+++ LogParser/Program.cs
@@ -4,41 +4,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data;
 using System.Xml;
-
-
-
 
 namespace LogParser
 {
     
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogList parser = new LogList();           
+            var temp = from e in parser.init where (!e.IsBot() && e.IsReal())  select e;
+            parser.init = temp.ToList();
+            
+            parser.Filter(parser.init);
+            //parser.FindAllCountries();
+            var temp2 = from e in parser.filtered group e by e.OS into g select new { Name = g.Key, Count = g.Count() };
+            temp2.ToList();
+            foreach (var e in temp2)
+            {
+                Console.WriteLine("{0} : {1}", e.Name, e.Count);
+            }
+            //parser.PrintFiltered();
+
+            //parser.init = logswithoutbots.ToList();
+            
+            
+            Console.ReadKey();            
+        }
+    }
+
+
+
     public class LogEntity
     {
+        public readonly static string [] OSList  = 
+            { "Windows NT 6.1", "Windows NT 6.2", "Windows NT 6.3", "Windows NT 10.0", "Android 6", "Android 4", "Android 5", "Windows NT 5.1", "IPhone", "Mac OS X"  } ; 
+        public LogEntity()
+        { }
+
         public string IP;
         public DateTime datetime;
         public bool Bot;
         public string OS;
         public string Browser;
         public string Country;
-        public LogEntity (DataRow logrow)
+        public string UA;
+        public string GetBrowser(string ua)
         {
-            IP = logrow["IP_Address"].ToString();
-            datetime = DateTime.ParseExact(logrow["DateTime"].ToString(), "dd/MMM/yyyy:H:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
-            Browser = Regex.Match(logrow["Browser"].ToString(), @"\D*").Groups[1].ToString();
-            OS = logrow["OS"].ToString();
-            Country = String.Empty;
-            Bot = logrow["UserAgent"].ToString().ToLower().Contains("bot") || logrow["UserAgent"].ToString().ToLower().Contains("mediapartners-google");// || logrow["UserAgent"].ToString().ToLower().Contains("http://");
+            var array_ua = ua.Split(' ');
+            string res = Regex.Match(array_ua.Last(), @"\D+").Groups[0].Value;
+            return res.Substring(0, res.Length-1) ;
         }
-        public void FindCountryByIP()
+        public string GetOS(string ua)
         {
-            Country = GetCountryByIP(IP);
-        }
-        public void Print ()
-        {
-            Console.WriteLine($"{IP}\t{Bot}\t{datetime}\t{OS}\t{Browser}\t{Country}");
+            var st = Regex.Matches(ua, @"\(([^)]*)\)");
+            for (int i = 0; i < st.Count; i++)
+            {
+                foreach (var os in OSList)
+                {                     
+                    if (st[i].Groups[1].Value.ToLower().Contains(os.ToLower()))
+                        return os;
+                }
+            }
+            return "No OS";
             
         }
+        public bool IsBot(string ua) => ua.ToLower().Contains("bot") || ua.ToLower().Contains("mediapartners-google");
+
+        public LogEntity(InitLogEntity init)
+        {
+            IP = init.IP;
+            datetime = ParseDateTime(init.Time);
+            Browser = GetBrowser(init.UA);
+            OS = GetOS(init.UA);
+            Country = String.Empty;
+            Bot = IsBot(init.UA);
+            //UA = init.UA;
+        }
+        public static DateTime ParseDateTime(string sditetime) => DateTime.ParseExact(sditetime.ToString(), "dd/MMM/yyyy:H:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+
+        public void FindCountryByIP() => Country = GetCountryByIP(IP);
+        public void Print() => Console.WriteLine($"{IP}\t{Bot}\t{datetime}\t{OS}\t{Browser}\t{Country}\t{UA}");
+
         static public string GetCountryByIP(string IP)
         {
 
@@ -51,133 +101,68 @@ namespace LogParser
                 string attr = node.InnerText;
                 return attr;
             }
-
         }
 
     }
-    public class AccessLogParser
+    public class InitLogEntity
     {
-         
-        public static  readonly string path = @"access.log";
-        private int records;
-        public DataTable initdt;
-         
-        public int BotsCount { get; private set; }
-        public int RealRequestCount { get; private set; }
-        public IEnumerable<DataRow> RealRequests { get => from row in initdt.AsEnumerable() where row["Req"].ToString().StartsWith("/book") select row; }
-        public IEnumerable<DataRow> Bots { get => from row in RealRequests where (row["UserAgent"].ToString().ToLower().Contains("bot")) select row; }
-        public IEnumerable<DataRow> FilteredLog { get; private set; }
-        public List<LogEntity> ToList(IEnumerable<DataRow> final)
-        {
-            List<LogEntity> res = new List<LogEntity>();
-            foreach (var e in final)
-            {
-
-                res.Add(new LogEntity(e));
-            }
-            return res;
-        }
-        public AccessLogParser ()
-        {
-            initdt = new DataTable();            
-            initdt.Columns.Add("IP_Address", typeof(string));
-            initdt.Columns.Add("DateTime", typeof(string));
-            initdt.Columns.Add("Req", typeof(string));
-            initdt.Columns.Add("Ref", typeof(string));
-            initdt.Columns.Add("UserAgent", typeof(string));
-            initdt.Columns.Add("OS", typeof(string));
-            initdt.Columns.Add("Browser", typeof(string));
-            initdt.Columns.Add("Country", typeof(string));
-            
-            System.IO.StreamReader file =  new System.IO.StreamReader(path);
-            string line;
-            while ((line = file.ReadLine())!=null)
-            {
-                
-                string[] lineArray = line.Split('"');
-
-                DataRow dr = initdt.NewRow();
-                string[] line0array = lineArray[0].Split(' ');
-                //string country = GetCountryByIP(line0array[0]);
-                //dr["Country"] = String.Format("{0, -40}", country);
-                dr["IP_Address"] =  line0array[0];
-                dr["DateTime"] =  line0array[3].Substring(1);
-                line0array = lineArray[1].Split(' ');
-                dr["Req"] =  line0array[1];
-                dr["Ref"] =  lineArray[3];
-                dr["UserAgent"] = lineArray[5];
-
-                var st = Regex.Matches(lineArray[5], @"\(([^)]*)\)");
-                if (st.Count >= 1)
-                    dr["OS"] = st[0].Groups[1];
-                var array_ua = lineArray[5].Split(' ');
-                dr["Browser"] = array_ua.Last();
-                initdt.Rows.Add(dr);
-                initdt.AcceptChanges();
-                records++;
-            }
-            file.Close();
-            
-
-        }
-        private void CalcReal() =>  RealRequestCount = RealRequests.Count();
-        private void CalcBots() => BotsCount = Bots.Count();
-        public IEnumerable<DataRow> Filer()
-        {
-            FilteredLog = RealRequests.GetNonBot();
-            return FilteredLog;
-        }
-        
-
-
+        public string IP;
+        public string Time;
+        public string status;
+        public string Req;
+        public string Ref;
+        public string UA;
+        public void Print() => Console.WriteLine($"{IP}\t{Time}\t{status}\t{Req}\t{Ref}\t{UA}");
+        public bool IsBot() => UA.ToLower().Contains("bot") || UA.ToLower().Contains("mediapartners-google");
+        public bool IsReal() => Req.StartsWith("/book");
     }
-    static class EnumExtentions
-    {
-        static public IEnumerable<DataRow> GetNonBot(this IEnumerable<DataRow> dt) => from row in dt where !(row["UserAgent"].ToString().ToLower().Contains("bot")) select row;
-
-        static public IEnumerable<DataRow> GetRealReq(this IEnumerable<DataRow> dt) => from row in dt where row["Req"].ToString().StartsWith("/book") select row;
-
-        static public int  GetBotPer(this IEnumerable<DataRow> dt)
-        {
-            var bots = (from row in dt.GetRealReq() where (row["UserAgent"].ToString().ToLower().Contains("bot")) select row).Count();      
-            
-            return bots;
-        }
-        
-
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            AccessLogParser parser = new AccessLogParser();
-            LogList list = new LogList();
-            LogList.init =  parser.ToList(parser.initdt.AsEnumerable());
-            Console.WriteLine(LogList.init.Count());
-            var logswithoutbots = from e in LogList.init where e.Bot select e;
-            list.filtered = logswithoutbots.ToList();
-            //list.FindAllCountries();
-            list.Print();
-            Console.WriteLine(list.filtered.Count());
-            var res = from e in list.filtered group e by e.IP;
-            
-            Console.ReadKey();
-            
-        }
-
-    }
-
     public class LogList
     {
-        
-        public static  List<LogEntity> init;
-        public  List<LogEntity> filtered;
+         
+        public List<InitLogEntity> init;
+        public List<LogEntity> filtered;
+        public readonly string path = @"access.log";
+        public LogList ()
+        {           
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            string line;
+            init = new List<InitLogEntity>();
+            InitLogEntity init_temp; ;
+            while ((line = file.ReadLine()) != null)
+            {
+                init_temp = new InitLogEntity();
+                string[] lineArray = line.Split('"');                                
+                string[] line0array = lineArray[0].Split(' ');
+
+                init_temp.IP = line0array[0];
+                init_temp.Time = line0array[3].Substring(1);
+                line0array = lineArray[1].Split(' ');
+                init_temp.Req = line0array[1];
+                init_temp.Ref = lineArray[3];
+                init_temp.UA = lineArray[5];
+                init_temp.status = lineArray[2].Split(' ')[0];
+                init.Add(init_temp);
+            }
+            file.Close();
+        }
+        public void Filter(List<InitLogEntity> p_init)
+        {
+            
+            filtered = new List<LogEntity>();            
+            foreach (var e in p_init)
+            {
+                filtered.Add(new LogEntity(e));
+            }
+        }
+       
         public  void FindAllCountries (  )
         {
-            filtered.ForEach(x => x.FindCountryByIP());
+            filtered.ForEach(x =>  x.FindCountryByIP());
 
         }
+        public static void Print(List<InitLogEntity> inp) => inp.ForEach(x => x.Print());
         public static void Print(List<LogEntity> inp ) => inp.ForEach(x => x.Print());
-        public  void Print() => filtered.ForEach(x => x.Print());
+        public  void PrintFiltered() => filtered.ForEach(x => x.Print());
+        public void PrintInit() => init.ForEach(x => x.Print());
     }
 }
